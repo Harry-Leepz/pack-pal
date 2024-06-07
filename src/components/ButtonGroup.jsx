2fea135cc933943018d6d1e13cfb422826ed0c64
Harry Dhillon <hdhillon478@gmail.com>
2024-06-07 11:21:14 +0100

    add button component to button group

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,3 +1,12 @@
+import Button from "./Button";
+
 export default function ButtonGroup() {
-  return <div>ButtonGroup</div>;
+  return (
+    <section className='button-group'>
+      <Button type='secondary' label='Mark all as complete' />
+      <Button type='secondary' label='Mark all as incomplete' />
+      <Button type='secondary' label='Reset to initial' />
+      <Button type='secondary' label='Remove all items' />
+    </section>
+  );
 }
