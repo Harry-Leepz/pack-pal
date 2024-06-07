8a9bd595f8e6db6cefad971cb0b0790f72a92007
Harry Dhillon <hdhillon478@gmail.com>
2024-06-07 17:00:34 +0100

    add logic to add new items to list

--- src/components/sidebar/AddItemForm.jsx
+++ src/components/sidebar/AddItemForm.jsx
@@ -1,10 +1,42 @@
+import { useState, useRef } from "react";
 import Button from "./Button";
 
-export default function AddItemForm() {
+export default function AddItemForm({ setItems }) {
+  const [itemName, setItemName] = useState("");
+  const inputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // validation
+    if (!itemName.trim()) {
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem = { id: new Date().getTime(), name: itemName, packed: false };
+    setItems((prevItems) => {
+      return [...prevItems, newItem];
+    });
+
+    // clear input and set focus
+    setItemName("");
+    inputRef.current.focus();
+  };
+
+  const changeHandler = (event) => {
+    setItemName(event.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <h2>Add an item</h2>
-      <input />
+      <input
+        ref={inputRef}
+        autoFocus={true}
+        placeholder='Item name...'
+        value={itemName}
+        onChange={changeHandler}
+      />
       <Button label='Add item to list' />
     </form>
   );
